14151e397e584eb7059e6874ffb61b52f540156e
Alex Baden <alex.baden@gmail.com>
2017-05-23 17:45:20 -0400

    Feat(datasource/boss): JHUAPL Boss datasource

--- src/neuroglancer/ui/default_viewer_setup.ts
+++ src/neuroglancer/ui/default_viewer_setup.ts
@@ -17,7 +17,7 @@
 import {makeDefaultKeyBindings} from 'neuroglancer/default_key_bindings';
 import {makeDefaultViewer} from 'neuroglancer/default_viewer';
 import {bindDefaultCopyHandler, bindDefaultPasteHandler} from 'neuroglancer/ui/default_clipboard_handling';
-import {UrlHashBinding} from 'neuroglancer/ui/url_hash_binding';
+import {UrlHashBinding, parseHashFromQueryString} from 'neuroglancer/ui/url_hash_binding';
 
 /**
  * Sets up the default neuroglancer viewer.
@@ -25,6 +25,8 @@ import {UrlHashBinding} from 'neuroglancer/ui/url_hash_binding';
 export function setupDefaultViewer() {
   let viewer = (<any>window)['viewer'] = makeDefaultViewer();
   makeDefaultKeyBindings(viewer.keyMap);
+
+  parseHashFromQueryString();
 
   const hashBinding = viewer.registerDisposer(new UrlHashBinding(viewer.state));
   hashBinding.updateFromUrlHash();
